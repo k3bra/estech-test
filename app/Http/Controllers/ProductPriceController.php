a9147bf4913e946a1a5590db8d9918f8d82791ff
eduardo <domingues.m.eduardo@gmail.com>
2020-07-29 09:42:12 +0100

    change output of api

--- app/Http/Controllers/ProductPriceController.php
+++ app/Http/Controllers/ProductPriceController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 use App\Account;
 use App\Product;
 use App\Services\FetchPrices;
+use Illuminate\Filesystem\Cache;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -17,7 +18,16 @@ class ProductPriceController extends Controller
      *
      *
      *
+     * retrieving from database with no account_id
      * 127.0.0.1:8000/prices?product_code[]=XBIMAL&product_code[]=DLWWXS&product_code[]=TAUXLC
+     *
+     *
+     * retrieving  from database with account_id
+     * 127.0.0.1:8000/prices?product_code[]=XBIMAL&product_code[]=DLWWXS&product_code[]=TAUXLC&account_id=217
+     *
+     *
+     * retrieving from live_prices for DLWWXS
+     * 127.0.0.1:8000/prices?product_code[]=XBIMAL&product_code[]=DLWWXS&product_code[]=TAUXLC&account_id=269
      *
      *
      *
@@ -27,7 +37,6 @@ class ProductPriceController extends Controller
      */
     public function index(Request $request)
     {
-
         $validator = \Validator::make($request->all(),
             [
                 'product_code' => 'required|array|min:1',
@@ -35,18 +44,19 @@ class ProductPriceController extends Controller
             ]
         );
 
-
-
-
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
 
-
         $fetchPricesSrv = new FetchPrices();
 
+        $productPrices = $fetchPricesSrv->fetch($request->get('product_code'), $request->get('account_id'));
+        $response = [];
 
-        return $fetchPricesSrv->fetch($request->get('product_code'), $request->get('account_id'));
+        foreach ($productPrices as $productCode => $price) {
+            $response[] = ['code' => $productCode, 'price' => $price];
+        }
 
+        return $response;
     }
 }
--- app/Services/FetchPrices.php
+++ app/Services/FetchPrices.php
@@ -5,6 +5,7 @@ namespace App\Services;
 
 
 use App\Account;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 
@@ -18,7 +19,7 @@ class FetchPrices
 
         foreach ($livePrices as $livePrice) {
 
-            if ($this->productCodeMatches($product, $livePrice) && $this->accountMatches($account, $livePrice))  {
+            if ($this->productCodeMatches($product, $livePrice) && $this->accountMatches($account, $livePrice)) {
 
                 $price = $price === null || $livePrice['price'] < $price ? $livePrice['price'] : $price;
 
@@ -44,9 +45,8 @@ class FetchPrices
         return array_key_exists('account', $livePrice) && $livePrice['account'] == $account;
     }
 
-    public function fetch(array $productCodes, ?int $accountID)
+    public function fetch(array $productCodes, ?int $accountID): array
     {
-
         $accountReference = $accountID ? Account::find($accountID)->external_reference : null;
 
         $livePrices = [];
@@ -61,17 +61,18 @@ class FetchPrices
 
         $livePriceProductCodes = array_keys($livePrices);
 
-         $productCodesWithNoPrice = array_diff($productCodes, $livePriceProductCodes);
+        $productCodesWithNoPrice = array_diff($productCodes, $livePriceProductCodes);
 
-         $databasePrices = !empty($productCodesWithNoPrice) ?  $this->fetchDatabasePrices($productCodesWithNoPrice, $accountID) : [];
+        $databasePrices = !empty($productCodesWithNoPrice) ? $this->fetchDatabasePrices($productCodesWithNoPrice, $accountID) : [];
 
-         return array_merge($livePrices, $databasePrices);
-
+        return array_merge($livePrices, $databasePrices);
     }
 
+    public function fetchDatabasePrices(array $productCodes, ?int $accountID): array
+    {
 
-    public function fetchDatabasePrices(array $productCodes, ?int $accountID) {
-
+        // $key = implode($productCodes) . '_' . 'account_id';
+        //return Cache::remember($key, 86400, function () use  {
 
         $sql = DB::table('prices')
             ->join('products', 'products.id', '=', 'prices.product_id')
@@ -86,7 +87,6 @@ class FetchPrices
 
         $sql->groupBy('prices.product_id');
 
-       return  $sql->get()->pluck('output_price', 'sku')->toArray();
-
+        return $sql->get()->pluck('output_price', 'sku')->toArray();
     }
 }
